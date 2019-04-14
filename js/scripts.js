8d9228182e2f8ecf502259c9f69aedd42660e835
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-04-13 22:32:25 -0700

    Add changes to business and UI logic, changed UI

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,7 @@ function Pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = toppings;
-  this.show = true;
 }
-
-// Constructor for order
-function Pizzas() {
-  this.pizzas = [];
-  // this.currentId = 0;
-}
-
-Pizzas.prototype.addPizza = function(pizza) {
-  // pizza.id = this.assignId();
-  this.pizzas.push(pizza);
-}
-
-var sizes = ["Small", "Medium", "Large"];
-var crusts = ["Original crust", "Gluten-friendly crust"];
 
 // Constructor for topping
 function Topping(name, price) {
@@ -30,39 +15,43 @@ function Topping(name, price) {
   this.price = price;
 }
 
+var sizes = ["Small", "Medium", "Large"];
+var crusts = ["Original crust", "Gluten-friendly crust"];
+
 var cheeseToppings = [
-  new Topping("Dairy-free cheese", 0.7),
-  new Topping("Feta", 0.5),
-  new Topping("Gorgonzola", 0.6),
-  new Topping("Mozzarella", 0.4),
-  new Topping("Parmesan", 0.6),
+  new Topping("Dairy-free cheese", 0.70),
+  new Topping("Feta", 0.50),
+  new Topping("Gorgonzola", 0.60),
+  new Topping("Mozzarella", 0.40),
+  new Topping("Parmesan", 0.60),
 ];
 
 var veggieToppings = [
-  new Topping("Artichokes", 0.4),
-  new Topping("Black olives", 0.4),
-  new Topping("Jalapenos", 0.2),
-  new Topping("Mushrooms", 0.2),
-  new Topping("Red peppers", 0.2),
-  new Topping("Spinach", 0.2),
-  new Topping("Tomatoes", 0.2),
+  new Topping("Artichokes", 0.40),
+  new Topping("Black olives", 0.40),
+  new Topping("Jalapenos", 0.20),
+  new Topping("Mushrooms", 0.20),
+  new Topping("Red peppers", 0.20),
+  new Topping("Spinach", 0.20),
+  new Topping("Tomatoes", 0.20),
 ];
 
 var meetToppings = [
-  new Topping("Anchovies", 1.5),
-  new Topping("Bacon", 1.8),
-  new Topping("Grilled chicken", 1.5),
-  new Topping("Pepperoni", 1.6),
-  new Topping("Salami", 1.5),
+  new Topping("Anchovies", 1.50),
+  new Topping("Bacon", 1.80),
+  new Topping("Grilled chicken", 1.50),
+  new Topping("Pepperoni", 1.60),
+  new Topping("Salami", 1.50),
 ];
 
 var sauceToppings = [
-  new Topping("BBQ swirl", 0.4),
-  new Topping("Hot buffalo sauce", 0.4),
-  new Topping("Ranch finish", 0.4),
-  new Topping("Pesto drizzle", 0.4),
+  new Topping("BBQ swirl", 0.40),
+  new Topping("Hot buffalo sauce", 0.40),
+  new Topping("Ranch finish", 0.40),
+  new Topping("Pesto drizzle", 0.40),
 ];
 
+var allToppings = [cheeseToppings, veggieToppings, meetToppings, sauceToppings];
 
 Pizza.prototype.calculatePrice = function(basePrice) {
   var price = basePrice;
@@ -81,7 +70,7 @@ Pizza.prototype.calculatePrice = function(basePrice) {
   if (crust == "Original crust") {
     price = price + 3
   } else {
-    price = price + 3.5
+    price = price + 3.50
   }
 
   toppings.forEach(function(topping) {
@@ -90,10 +79,17 @@ Pizza.prototype.calculatePrice = function(basePrice) {
   return price
 }
 
-
 // User Interface Logic
 
 $(document).ready(function() {
+  $("#pizza").hide();
+
+  $("#pizza-image").click(function(event) {
+    event.preventDefault();
+    $("#pizza-image").hide();
+    $("#start").hide();
+    $("#pizza").show();
+
   cheeseToppings.forEach(function(t) {
     $("#cheese-check-label").show();
     $("#cheese").append('<input type="checkbox" name="cheese" value="' + t.name + '">' + t.name + '<br>');
@@ -110,31 +106,39 @@ $(document).ready(function() {
     $("#sauce-check-label").show();
     $("#sauce").append('<input type="checkbox" name="sauce" value="' + t.name + '">' + t.name + '<br>');
   });
-  
+});
+
   $("#pizza").submit(function(event) {
     event.preventDefault();
+    var chosenSize = $("input:radio[name=size]:checked").val();
+    var chosenCrust = $("input:radio[name=crust]:checked").val();
+    var chosenToppingsNames = [];
+    $("input[type=checkbox]:checked").each(function() {
+      chosenToppingsNames.push($(this).val());
+    });
 
+    var chosenToppings = [];
+    chosenToppingsNames.forEach(function(toppingName) {
+      allToppings.forEach(function(toppingCategory) {
+        toppingCategory.forEach(function(topping) {
+          if (topping.name == toppingName) {
+            chosenToppings.push(topping)
+          }
+        });
+      });
+    });
 
-    //
-    // var chosenSize = $("input:radio[name=size]:checked").val();
-    // var chosenToppingsNames = [];
-    // $("input:checked").each(function() {
-    //   chosenToppingsNames.push($(this).val());
-    // });
-    // var chosenToppings = [];
-    // chosenToppingsNames.forEach(function(toppingName) {
-    //   allToppings.forEach(function(toppingCategory) {
-    //     toppingCategory.forEach(function(topping) {
-    //       if (topping.name == toppingName) {
-    //         chosenToppings.push(topping)
-    //       }
-    //     });
-    //   });
-    // });
-    //
-    // var basePrice = 5;
-    // var pizza =  new Pizza (chosenSize, chosenToppings);
-    // var pizzaPrice = pizza.calculatePrice(basePrice);
-    // $("#pizzaPrice").text("Your pizza is " + chosenSize + " size with " + chosenToppingsNames + " toppings is cost " + "$" + pizzaPrice);
+    var basePrice = 5;
+    var pizza =  new Pizza (chosenSize, chosenCrust, chosenToppings);
+    var pizzaPrice = pizza.calculatePrice(basePrice);
+    var pizzaPriceRound = pizzaPrice.toFixed(2);
+    var chosenPizzas = [];
+    chosenPizzas.push(pizza);
+    $('#pizza-panels').append(
+      '<div id="pizza-panel-' + '"class="panel panel-default"><div class="panel-heading"><h4 class="panel-title">' +"Your pizza: "  + '</h4></div><div class="panel-body"><p> ' + "Size: " + chosenSize + '<br>' + "Crust: " + chosenCrust + "<br>" + "Toppings: " + chosenToppingsNames.join(', ') + "<br>" + "Price: "  + pizzaPriceRound + '</p></div></div>'
+    );
+    $('input[type=radio]').prop('checked', false);
+    $('input[type=checkbox]').prop('checked', false);
+
   });
 });
